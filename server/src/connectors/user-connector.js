65bd0f350c130b0adf371814784c4df7c246ad7a
Kumar <sidhartha.kumar@bp.com>
2020-03-02 14:44:44 -0600

    added mutation to schema

--- server/src/connectors/user-connector.js
+++ server/src/connectors/user-connector.js
@@ -11,12 +11,40 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
 const fetch = require('node-fetch');
 
 var userConnector = {
-    fetchUsers(){
-            const url = 'https://jsonplaceholder.typicode.com/users'
-            return fetch(url).then(response => response.json()).then(function(json){
-                return json
-            });
+  fetchUsers(){
+    const url = 'https://jsonplaceholder.typicode.com/users'
+    return fetch(url).then(response => response.json()).then(json =>json);
+  },
+    
+  getOne(Id){
+    const url = 'https://jsonplaceholder.typicode.com/users'
+    return fetch(url).then(response => response.json()).then(function(json){
+      for(var user in json){
+        if (json[user].id == Id){
+            return json[user];
         }
+      }
+      return null;
+    });
+  },
+
+  save(user, context){
+    return fetch('https://jsonplaceholder.typicode.com/users', {
+        method: 'POST',
+        body: JSON.stringify({
+            id: user.user.id,
+            name: user.user.name,
+            email: user.user.email,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      })
+      .then(function(response){
+          console.log(response.status);
+          return response.json();
+      }).then(json =>json);
+  }
 }
 
 
--- server/src/resolvers/user-resolver.js
+++ server/src/resolvers/user-resolver.js
@@ -7,6 +7,16 @@ var userResolver = {
     Query: {
         users(root, args, context){
             return userConnector.fetchUsers().then(users => users);
+        },
+
+        user(root, args, context){
+            return userConnector.getOne(args.id).then(user => user);
+        }
+    },
+
+    Mutation:{
+        createUser(parent, args, context){
+            return userConnector.save(args).then(user => user);
         }
     }
 }
